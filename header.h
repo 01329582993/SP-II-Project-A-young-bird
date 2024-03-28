baaaa9629b83dd96703f693a78926569937156b9
LIMASSOU HAMAN BELLO <149891433+01329582993@users.noreply.github.com>
2024-03-28 14:59:04 +0600

    Add files via upload
    THe first part of the young bird game , all about menu is there ...

---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+#endif // HEADER_H_INCLUDED
